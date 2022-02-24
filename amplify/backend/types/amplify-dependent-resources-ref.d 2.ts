41efa39cea1df31b1e29710a428f96d702913a28
AFURO-1130 <softtennis6161130@icloud.com>
2022-02-24 13:34:09 +0900

    ji

--- amplify/backend/types/amplify-dependent-resources-ref.d 2.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "nuxtamplify": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
