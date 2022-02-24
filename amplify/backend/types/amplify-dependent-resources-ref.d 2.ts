36929be07b3ed2a194f6c34146d971a6097b2262
AFURO-1130 <softtennis6161130@icloud.com>
2022-02-24 12:45:53 +0900

    hu

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d 2.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "nuxtamplify": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/graphql/mutations 2.js
+++ src/graphql/mutations 2.js
@@ -10,6 +10,7 @@ export const createTodo = /* GraphQL */ `
       id
       name
       description
+      isEdit
       createdAt
       updatedAt
     }
@@ -24,6 +25,7 @@ export const updateTodo = /* GraphQL */ `
       id
       name
       description
+      isEdit
       createdAt
       updatedAt
     }
@@ -38,6 +40,7 @@ export const deleteTodo = /* GraphQL */ `
       id
       name
       description
+      isEdit
       createdAt
       updatedAt
     }
